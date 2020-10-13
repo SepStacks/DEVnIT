9f9abb396c4cbf76519545e148ad40f70b544554
Jermaine <jermaine.sepkitt@bytes.co.za>
2020-10-13 11:27:30 +0200

    devided menus into groups and merged those into one with the same name

--- components/navigation/SideNav.vue
+++ components/navigation/SideNav.vue
@@ -1,6 +1,14 @@
 <template>
+  <div>
+    <v-app-bar app>
 
-    <v-navigation-drawer permanent app>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      permanent
+      app
+      clipped
+    >
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">
@@ -13,33 +21,72 @@
       </v-list-item>
 
       <v-divider></v-divider>
+<v-list nav dense>
+<div v-for="(header, index) in Object.keys(headers)" :key="index">
+  <div class="font-weight-bold">{{ header }}</div>
 
-      <v-list
+  <v-list-item v-for="(slug, innerIndex) in headers[header]" :key="innerIndex" :to="slug">
+    {{ slug === 'index' ? 'Installation' : slug }}
 
-        nav
-      >
+  </v-list-item>
+</div>
+</v-list>
+      <!-- <v-list nav>
         <v-list-item
-        nuxt
-        v-for="menu in menus"
-        :key="menu.slug"
-        :to="menu.path"
+          v-for="menu in headers"
+          :key="menu.slug"
+          :to="menu.path"
         >
 
 
           <v-list-item-content>
-            <v-list-item-title>{{ menu.slug }}</v-list-item-title>
+            <v-list-item-title>   <div>}</div></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
+      </v-list> -->
     </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
-  export default {
+export default {
   props: {
-      menus: {
-          type: Array
-      }
+    menus: {
+      type: Array
+    }
+  },
+
+  computed: {
+
+
+    headers () {
+
+
+      let ar = []
+
+      this.menus.forEach(el => {
+
+        ar.push({
+          title: el.title,
+          path: el.path,
+          slug: el.slug,
+          header: el.slug === 'index' ? 'Getting Started'
+            : el.slug === 'commits' ? 'Gitlab Commits' : "Components"
+        })
+      })
+
+
+      return ar.reduce((header, group) => {
+        if (header.hasOwnProperty(group.header) && Array.isArray(header[group.header])) {
+          header[group.header].push(group.slug)
+        } else {
+          Object.assign(header, { [group.header]: [group.slug] })
+        }
+        return header
+      }, {})
+
+
+    }
   }
-  }
+};
 </script>
---
+++ content/projects/BOS/forms.md
@@ -0,0 +1,33 @@
+# Forms
+>This component was based on the forms component of [Vuetify](https://v15.vuetifyjs.com/en/components/forms/ "Vuetify's form component"). Some properties of the text area component has been added aswell. The link to vuetify's text area can be found [here](https://v15.vuetifyjs.com/en/components/text-fields)
+
+## Usage
+
+Forms are usually used to excecute a certain action based on the user interaction. eg, a login form, a survey, a registration form etc.
+
+
+
+## Props
+
+| Name              | Default                       | Type              |
+| -------------     |:-------------:                | --------------:   |
+|color              | accent                        |   string          |
+|type               | text                          |   string          |
+|value              | true                          |   boolean         |
+
+
+## Examples
+
+### Search bar
+
+Using a filter function, a custom search bar can be created to search objects within an array.
+
+
+
+### Login Form
+Login forms can be used for all kinds of authentication purposes.
+
+
+
+
+
